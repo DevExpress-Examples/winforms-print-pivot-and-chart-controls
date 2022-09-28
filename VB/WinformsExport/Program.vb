bcde515c99e224e6dadb6ee08cf2a2dd67cc4414
DevExpressExampleBot <fakeEmail>
2022-09-28 16:24:51 +0400

    Source auto update [skip ci]

--- VB/WinformsExport/Program.vb
+++ VB/WinformsExport/Program.vb
@@ -1,21 +1,18 @@
-﻿Imports DevExpress.LookAndFeel
-Imports DevExpress.Skins
-Imports DevExpress.UserSkins
 Imports System
-Imports System.Collections.Generic
-Imports System.Linq
 Imports System.Windows.Forms
 
 Namespace WinFormsExport
-	Friend Module Program
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		<STAThread>
-		Sub Main()
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
-			Application.Run(New Form1())
-		End Sub
-	End Module
+
+    Friend Module Program
+
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Sub Main()
+            Call Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            Call Application.Run(New Form1())
+        End Sub
+    End Module
 End Namespace
